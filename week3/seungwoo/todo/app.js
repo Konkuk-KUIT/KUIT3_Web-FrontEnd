fdaafda36221862e6b71e6e192ebd2f1adea79a5
Seungwoo <2002msw@naver.com>
2024-04-05 22:56:44 +0900

    folder edit

--- week3/seungwoo/todo/app.js
+++ week3/seungwoo/todo/app.js
@@ -10,6 +10,37 @@ fetch(API_URL)
 const updateTodo = (todoId, originalTitle) => {
   const todoItem = document.querySelector(`#todo-${todoId}`);
   // mission
+  const editEl = document.createElement("input");
+  editEl.value = todoItem.firstChild.textContent;
+  todoItem.firstChild.textContent = "";
+
+  editEl.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      const inputValue = editEl.value;
+      console.log(inputValue);
+      if (editEl.value == "") {
+        deleteTodo(todoId);
+      } else {
+        editEl.remove();
+        todoItem.firstChild.textContent = inputValue;
+      }
+
+      //서버 데이터에 반영하는 부분 추가
+      fetch(API_URL + "/" + todoId, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: inputValue
+        }),
+      }).then(response => response.json())
+    }
+  });
+
+  todoItem.prepend(editEl);
+
+  console.log(todoItem);
 };
 
 const renderTodo = (newTodos) => {
@@ -41,7 +72,7 @@ const addTodo = () => {
   if (!title) return;
 
   const newTodo = {
-    id: date.getTime(),
+    id: date.getTime().toString(),
     title,
     createdAt,
   };
@@ -63,6 +94,7 @@ const addTodo = () => {
 };
 
 const deleteTodo = (todoId) => {
+  console.log(API_URL + "/" + todoId);
   fetch(API_URL + "/" + todoId, {
     method: "DELETE",
   })
