8b50df87215a61e0ebb92bf8c2ca851e84c4f319
yaho <rlatjwls2923@gmail.com>
2024-04-05 22:25:00 +0900

    week3 미션 준비

--- week3/seojin/app.js
+++ week3/seojin/app.js
@@ -7,6 +7,11 @@ fetch(API_URL)
   .then((response) => response.json())
   .then((data) => renderTodo(data));
 
+const updateTodo = (todoId, originalTitle) => {
+  const todoItem = document.querySelector(`#todo-${todoId}`);
+  // mission
+};
+
 const renderTodo = (newTodos) => {
   todoListEl.innerHTML = "";
   newTodos.forEach((todo) => {
@@ -19,39 +24,14 @@ const renderTodo = (newTodos) => {
     deleteEl.onclick = () => deleteTodo(todo.id);
 
     const udpateEl = document.createElement("span");
-    udpateEl.textContent = "수정하기";
+    udpateEl.textContent = "✏️";
     udpateEl.onclick = () => updateTodo(todo.id, todo.title);
 
-    icon.append(deleteEl);
-    icon.append(udpateEl);
-    listEl.append(icon);
+    listEl.append(deleteEl);
+    listEl.append(udpateEl);
     todoListEl.append(listEl);
   });
 };
-
-const todoItem = document.querySelector(`#todo-${todoId}`);
-todoItem.innerHTML = "";
-const inputEl = document.createElement("input");
-inputEl.id = "Input";
-inputEl.value = originalTitle;
-todoItem.append(inputEl);
-
-inputEl.focus();
-inputEl.addEventListener("keydown", (event) => {
-  if (event.key == "Enter") {
-    const newTitle = inputEl.value;
-    fetch(API_URL + "/" + todoId, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ title: newTitle }),
-    })
-      .then(() => fetch(API_URL))
-      .then((response) => response.json())
-      .then((data) => renderTodo(data));
-  }
-});
 
 const addTodo = () => {
   const title = todoInputEl.value;
