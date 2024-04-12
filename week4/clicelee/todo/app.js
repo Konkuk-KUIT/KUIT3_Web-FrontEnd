0f21c0c378ab862e6bf568b7361f3276f6b2c4f0
clice lee <cliceleee@gmail.com>
2024-04-12 22:53:00 +0900

    refactor: convert Javascript to Typescript

--- week4/clicelee/todo/app.js
+++ week4/clicelee/todo/app.js
@@ -1,96 +1,95 @@
-const todoListEl = document.getElementById("todoList");
-const todoInputEl = document.getElementById("todoInput");
-
-const API_URL = "http://localhost:8080/todos";
-
-fetch(API_URL)
-  .then((response) => response.json())
-  .then((data) => renderTodo(data));
-
-const renderTodo = (newTodos) => {
-  todoListEl.innerHTML = "";
-  newTodos.forEach((todo) => {
-    const listEl = document.createElement("li");
-    listEl.textContent = todo.title;
-    listEl.id = `todo-${todo.id}`;
-
-    const deleteEl = document.createElement("span");
-    deleteEl.textContent = "🗑️";
-    deleteEl.onclick = () => deleteTodo(todo.id);
-
-    const updateEl = document.createElement("span"); 
-    updateEl.textContent = "✏️";
-    updateEl.onclick = () => updateTodo(todo.id, todo.title); 
-
-    listEl.append(deleteEl);
-    listEl.append(updateEl);
-    todoListEl.append(listEl);
-  });
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-
-const updateTodo = (todoId, originalTitle) => {
-  const todoItem = document.querySelector(`#todo-${todoId}`);
-  const newTitle = prompt("Enter new title:", originalTitle);
-
-  if (!newTitle) return; 
-
-  fetch(API_URL + "/" + todoId, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ title: newTitle }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Failed to update todo");
-      }
-      return response.json();
-    })
-    .then(() => {
-      return fetch(API_URL);
-    })
-    .then((response) => response.json())
-    .then((data) => renderTodo(data))
-    .catch((error) => {
-      console.error("Error updating todo:", error.message);
+var todoListEl = document.getElementById("todoList");
+var todoInputEl = document.getElementById("todoInput");
+var API_URL = "http://localhost:8080/todos";
+fetch(API_URL)
+    .then(function (response) { return response.json(); })
+    .then(function (data) { return renderTodo(data); });
+var renderTodo = function (newTodos) {
+    todoListEl.innerHTML = "";
+    newTodos.forEach(function (todo) {
+        var listEl = document.createElement("li");
+        listEl.textContent = todo.title;
+        listEl.id = "todo-".concat(todo.id);
+        var deleteEl = document.createElement("span");
+        deleteEl.textContent = "🗑️";
+        deleteEl.onclick = function () { return deleteTodo(todo.id); };
+        var updateEl = document.createElement("span");
+        updateEl.textContent = "✏️";
+        updateEl.onclick = function () { return updateTodo(todo.id, todo.title); };
+        listEl.append(deleteEl);
+        listEl.append(updateEl);
+        todoListEl.append(listEl);
     });
 };
-
-const addTodo = () => {
-  const title = todoInputEl.value;
-  const date = new Date();
-  const createdAt = date.toDateString();
-
-  if (!title) return;
-
-  const newTodo = {
-    id: date.getTime(),
-    title,
-    createdAt,
-  };
-
-  fetch(API_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ ...newTodo, completed: false }),
-  })
-    .then((response) => response.json())
-    .then(() => {
-      todoInputEl.value = "";
-      return fetch(API_URL);
+var updateTodo = function (todoId, originalTitle) {
+    var todoItem = document.querySelector("#todo-".concat(todoId));
+    var newTitle = prompt("Enter new title:", originalTitle);
+    if (!newTitle)
+        return;
+    fetch(API_URL + "/" + todoId, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title: newTitle }),
     })
-    .then((response) => response.json())
-    .then((data) => renderTodo(data));
+        .then(function (response) {
+        if (!response.ok) {
+            throw new Error("Failed to update todo");
+        }
+        return response.json();
+    })
+        .then(function () {
+        return fetch(API_URL);
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) { return renderTodo(data); })
+        .catch(function (error) {
+        console.error("Error updating todo:", error.message);
+    });
 };
-
-const deleteTodo = (todoId) => {
-  fetch(API_URL + "/" + todoId, {
-    method: "DELETE",
-  })
-    .then(() => fetch(API_URL))
-    .then((response) => response.json())
-    .then((data) => renderTodo(data));
+var addTodo = function () {
+    var title = todoInputEl.value;
+    var date = new Date();
+    var createdAt = date.toDateString();
+    if (!title)
+        return;
+    var newTodo = {
+        id: date.getTime(),
+        title: title,
+        createdAt: createdAt,
+    };
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(__assign(__assign({}, newTodo), { completed: false })),
+    })
+        .then(function (response) { return response.json(); })
+        .then(function () {
+        todoInputEl.value = "";
+        return fetch(API_URL);
+    })
+        .then(function (response) { return response.json(); })
+        .then(function (data) { return renderTodo(data); });
+};
+var deleteTodo = function (todoId) {
+    fetch(API_URL + "/" + todoId, {
+        method: "DELETE",
+    })
+        .then(function () { return fetch(API_URL); })
+        .then(function (response) { return response.json(); })
+        .then(function (data) { return renderTodo(data); });
 };
