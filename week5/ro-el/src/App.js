f7b5e72c3b9e754a3404946c5f4b15d229ba54f2
ro-el-c <chung3286@naver.com>
2024-05-02 00:22:10 +0900

    feat: week5 미션 구현
    1. completed가 true인 값만 / 2. priority가 5 이상인 값만 / 둘 다 map으로

--- week5/ro-el/src/App.js
+++ week5/ro-el/src/App.js
@@ -16,19 +16,53 @@ const App = () => {
   return (
     <>
       <Header title={title} />
-      
+
       <h2>실습</h2>
       {/* <Todo todo={todoData.todos[0]} /> */}
-      {todoData.todos.map((value, index) => (
-        <Todo
-          key={value.id} //or key={index}
-          id={value.id}
-          task={value.task}
-          completed={value.completed}
-          priority={value.priority}
-        />
-      ))}
+      <ul className="todo-list">
+        {todoData.todos.map((value, index) => (
+          <Todo
+            key={value.id} //or key={index}
+            id={value.id}
+            task={value.task}
+            completed={value.completed}
+            priority={value.priority}
+          />
+        ))}
+      </ul>
+      <hr />
 
+      <h2>미션1: completed가 true인 값만 map으로 출력</h2>
+      <ul className="todo-list">
+        {todoData.todos.map((value, index) =>
+          value.completed ? ( //삼항 연산자
+            <Todo
+              key={value.id} //or key={index}
+              id={value.id}
+              task={value.task}
+              completed={value.completed}
+              priority={value.priority}
+            />
+          ) : null
+        )}
+      </ul>
+      <hr />
+
+      <h2>미션2: priority가 5 이상인 값만 map으로 출력</h2>
+      <ul className="todo-list">
+        {todoData.todos.map(
+          (value, index) =>
+            value.priority >= 5 && ( //단축 평가 논리 계산법
+              <Todo
+                key={value.id} //or key={index}
+                id={value.id}
+                task={value.task}
+                completed={value.completed}
+                priority={value.priority}
+              />
+            )
+        )}
+      </ul>
     </>
   );
 };
--- week5/ro-el/src/Todo.js
+++ week5/ro-el/src/Todo.js
@@ -2,13 +2,27 @@ import React from "react";
 
 const Todo = ({ id, task, completed, priority }) => {
   return (
-    <div>
-      <div>id : {id}</div>
-      <div>task : {task}</div>
-      <div>completed : {completed.toString()}</div>
-      <div>priority : {priority}</div>
-      <div>----------------------------</div>
-    </div> 
+    <>
+      <li className="todo">
+        <div>
+          <span className="key">id: </span>
+          <span className="value">{id}</span>
+        </div>
+        <div>
+          <span className="key">task: </span>
+          <span className="value">{task}</span>
+        </div>
+        <div>
+          <span className="key">completed: </span>
+          <span className="value">{completed.toString()}</span>
+        </div>
+        <div>
+          <span className="key">priority: </span>
+          <span className="value">{priority}</span>
+        </div>
+      </li>
+      <p></p>
+    </>
   );
 };
 
