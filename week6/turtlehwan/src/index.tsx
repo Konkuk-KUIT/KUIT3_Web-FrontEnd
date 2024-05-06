c734d5a23a8ab76a42edddad892fd84d2d757742
김지환 (Ji-hwan Kim) <kjhwan0802@naver.com>
2024-05-06 14:40:04 +0000

    add strictmode

--- week6/turtlehwan/src/index.tsx
+++ week6/turtlehwan/src/index.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
