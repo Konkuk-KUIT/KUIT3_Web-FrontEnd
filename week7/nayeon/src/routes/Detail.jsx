25518ffcce5f22d249901f1ad4cbb24e77fe4891
김나연 <apinksky00@gmail.com>
2024-05-09 20:09:30 +0900

    feat: week7 코드

---
+++ week7/nayeon/src/routes/Detail.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "../components/Header";
+import { data } from "../db";
+import { Link } from "react-router-dom";
+
+const Detail = () => {
+  return (
+    <>
+      <Header />
+      {data.map((data, index) => (
+        <div key={index}>
+          <Link to={`/detail/${index}`}>{data.name}</Link>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Detail;
