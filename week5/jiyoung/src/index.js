2754b284d8933248995477445730723d72d0c153
Daljyeong <jiyoungm1022@gmail.com>
2024-05-10 21:30:12 +0900

    remove: 경로 잘못된 파일 삭제

--- week5/jiyoung/src/index.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const root = createRoot(document.getElementById("app")); // root 지정
-
-// root rendering
-// root.render(<h1>Hello!</h1>);
-root.render(<App />);
