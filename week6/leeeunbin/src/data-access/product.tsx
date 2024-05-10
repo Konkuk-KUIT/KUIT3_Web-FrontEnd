51b09705fc9c8e1af7373f071e8274cffa824648
leeeunbin <silverbin2105@gmail.com>
2024-05-10 18:20:46 +0900

    feat. 6주차 미션

---
+++ week6/leeeunbin/src/data-access/product.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Products {
+  category : string;
+  price : string;
+  stocked : boolean;
+  name : string;
+  // onDelete: (product: Products) => void;
+  // onEdit: (product: Products) => void;
+}
+
+export default Products;
