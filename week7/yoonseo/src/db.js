34589a1ee2eeb642ecb7de714f35ec922f8df424
yskim6772 <yskim6772@gmail.com>
2024-05-14 03:22:23 +0900

    Feat: 7주차 실습

---
+++ week7/yoonseo/src/db.js
@@ -0,0 +1,5 @@
+export const data = [
+    {name: "김윤서0", age: "23살0", description: "벌써 스물셋이라니....0"},
+    {name: "김윤서1", age: "23살1", description: "벌써 스물셋이라니....1"},
+    {name: "김윤서2", age: "23살2", description: "벌써 스물셋이라니....2"},
+];
