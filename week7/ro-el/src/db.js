5082d586a4756a555c448a7f416f2d517f1c85ba
ro-el <chung3286@naver.com>
2024-05-16 19:58:44 +0900

    docs: update db

--- week7/ro-el/src/db.js
+++ week7/ro-el/src/db.js
@@ -4,4 +4,8 @@ export const data = [
   { name: "정연우", age: 22, description: "너도 귀여워 볼 조믈딱" },
   { name: "조용찬", age: 26, description: "오빤 짱 커 눈썹 진해" },
   { name: "장우진", age: 25, description: "아프지마ㅜ" },
+  { name: "김별찬", age: 25, description: "우리 튜터가 최고야 :) 똑똑이" },
+  { name: "조하상", age: 25, description: "그만 와. 팁이 너무 길어" },
+  { name: "김나연", age: 25, description: "넌 좀 와줘. 보고싶다 ^~^" },
+  { name: "전진호", age: 24, description: "넌... 뭐야?" },
 ];
