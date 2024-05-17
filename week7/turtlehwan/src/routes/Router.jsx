1ecc252ccf1f64e9bdf633fa99bfeded1dba7b04
김지환 (Ji-hwan Kim) <kjhwan0802@naver.com>
2024-05-17 13:30:28 +0000

    feat. 7주차 과제 : 카드 홀로그램 모션

--- week7/turtlehwan/src/routes/Router.jsx
+++ week7/turtlehwan/src/routes/Router.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Detail from "./Detail";
-import User from "./User";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/detail" element={<Detail />} />
-      <Route path="/detail/:id" element={<User />} />
+      <Route path="/detail/:id" element={<Detail />} />
     </Routes>
   );
 };
