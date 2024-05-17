e3cfcb7eed1a502c7a450731c51e453db387b10a
leeeunbin <silverbin2105@gmail.com>
2024-05-17 12:15:25 +0900

    feat. 7주차 미션

---
+++ week7/leeeunbin/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #000;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+
+  &:hover {
+    color: #007bff;
+  }
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  gap: 20px;
+  padding: 10px;
+  background-image : url('/image/header.jpg');
+  background-size : cover;
+  background-position : center;
+  border-bottom: 1px solid #dee2e6;
+`;
+
+const Header = () => {
+  return (
+    <StyledDiv>
+      <StyledLink to="/">
+        <FontAwesomeIcon icon={faHome} />
+        홈
+      </StyledLink>
+      <StyledLink to="/detail">
+        <FontAwesomeIcon icon={faInfoCircle} />
+        상세
+      </StyledLink>
+    </StyledDiv>
+  );
+};
+
+export default Header;
---
+++ week7/leeeunbin/src/db.js
@@ -0,0 +1,5 @@
+export const data = [
+  { image : "/image/user1.webp", name: "김나연0", age: "25살0", description: "벌써 반오십이라니....0" },
+  { image : "/image/user2.webp", name: "김나연1", age: "25살1", description: "벌써 반오십이라니....1" },
+  { image : "/image/user3.jpg", name: "김나연2", age: "25살2", description: "벌써 반오십이라니....2" },
+];
