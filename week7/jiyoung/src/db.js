be33e223a578e832367603dfa7de524cf42007f4
Daljyeong <jiyoungm1022@gmail.com>
2024-05-17 15:44:29 +0900

    feat: 홈/상세 페이지 기본 구현 (KUIT 3기 Web week7 실습)

---
+++ week7/jiyoung/src/db.js
@@ -0,0 +1,5 @@
+export const data = [
+    { name: "문지영0", age: "23살0", description: "안녕하세요0" },
+    { name: "문지영1", age: "23살1", description: "안녕하세요1" },
+    { name: "문지영2", age: "23살2", description: "안녕하세요2" },
+]
