55c52091a9f3e0c05be4e06544063de961495ef9
Daljyeong <jiyoungm1022@gmail.com>
2024-05-24 19:17:48 +0900

    design: 'MenuItem.jsx' 스타일링 완료 (KUIT 3기 Web week8 미션)

--- week8/jiyoung/src/components/MenuItem/MenuItem.jsx
+++ week8/jiyoung/src/components/MenuItem/MenuItem.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 import useCartStore from "../../store/useCartStore";
+import {
+  StyledMenuBox,
+  StyledMenuLeftDiv,
+  StyledMenuImg,
+  StyledMenuTv,
+  StyledMenuName,
+  StyledMenuPrice,
+  StyledMenuIng,
+  StyledMenuBtn,
+} from "./MenuItem.styles";
 
 const MenuItem = ({ menu }) => {
   const addMenu = useCartStore((state) => state.addMenu);
@@ -8,14 +18,20 @@ const MenuItem = ({ menu }) => {
   };
 
   return (
-    <div>
-      <h3>{menu.name}</h3>
-      <span>{menu.price}</span>
-      <p>{menu.ingredients}</p>
-      <button onClick={handleAddMenu} type="button">
+    <StyledMenuBox>
+      <StyledMenuLeftDiv>
+        <StyledMenuImg />
+        <StyledMenuTv>
+          <StyledMenuName>{menu.name}</StyledMenuName>
+          <StyledMenuPrice>{menu.price}원</StyledMenuPrice>
+          <StyledMenuIng>{menu.ingredients}</StyledMenuIng>
+        </StyledMenuTv>
+      </StyledMenuLeftDiv>
+
+      <StyledMenuBtn onClick={handleAddMenu} type="button">
         담기
-      </button>
-    </div>
+      </StyledMenuBtn>
+    </StyledMenuBox>
   );
 };
 
