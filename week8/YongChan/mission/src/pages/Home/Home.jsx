9be566f7949e16405362e69f82c1cfea90d25e74
jyc0328 <sykc2002@gmail.com>
2024-05-26 23:19:32 +0900

    Feat:8주차 미션

---
+++ week8/YongChan/mission/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
