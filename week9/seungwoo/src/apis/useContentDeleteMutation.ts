2dabb25aa8b5c8375ec1d32f6c25b583c989aba9
seungwoo <2002msw@naver.com>
2024-05-31 11:41:13 +0900

    seungwoo folder created

---
+++ week9/seungwoo/src/apis/useContentDeleteMutation.ts
@@ -0,0 +1,7 @@
+// 미션: 글 삭제 시 DELETE mutation hook 구현하기
+
+// 삭제 버튼을 눌렀을 때 mutation
+// 삭제 버튼을 눌렀을 때 -> 삭제 버튼 누르고 useNavigate 사용해서 board로 이동
+// 1. apis/useContentDeleteMutation.ts 구현
+// 2. pages/Content.tsx에서 handleContentDeleteClick 구현
+// 3. organisms/ContentView.tsx도 코드 추가
