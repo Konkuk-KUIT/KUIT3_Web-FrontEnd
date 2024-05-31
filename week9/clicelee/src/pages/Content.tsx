261e6c2ebfbef8b00dc1a47dc3557f1a3bc68a3a
clice lee <cliceleee@gmail.com>
2024-05-31 16:45:24 +0900

    feat:DELETE mutation hook -Content.tsx: add import useNavigate

--- week9/clicelee/src/pages/Content.tsx
+++ week9/clicelee/src/pages/Content.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+
 
 import DarkModeToggle from '../components/atoms/DarkModeToggle';
 
