afde14d87e18c89e98aaceaac768c8d2c036c081
HS <77558413+crohasang@users.noreply.github.com>
2024-06-04 17:04:11 +0900

    Revert "feat: 9주차 미션 완료"

--- week9/seojin/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.jpg';
-declare module '*.png';
-declare module '*.jpeg';
-declare module '*.gif';
-
-declare module '*.css' {
-  const content: { [className: string]: string };
-  export default content;
-}
